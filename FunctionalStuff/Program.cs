b33767b131a388ccdf88c807d55c6e3fa8ba306c
Jlobblet <jlobblet@gmx.com>
2021-02-27 15:36:08 +0000

    Initial commit

---
+++ FunctionalStuff/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using FunctionalStuff.Option;
+
+namespace FunctionalStuff
+{
+    public static class Program
+    {
+        private static Option<int> TryDivide(int num, int den)
+        {
+            if (den == 0)
+            {
+                return new None<int>();
+            }
+
+            return new Some<int>(num / den);
+        }
+
+        static void Main(string[] args)
+        {
+            var contentPackage =
+                new ContentPackage("cp.xml");
+            
+            Console.WriteLine("hi");
+        }
+    }
+}
