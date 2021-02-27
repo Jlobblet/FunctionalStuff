52daf46fc2a12d26daf70305a1f0893ce864b159
Jlobblet <jlobblet@gmx.com>
2021-02-27 16:42:40 +0000

    Flesh out the Option class

--- FunctionalStuff/Option/Option.cs
+++ FunctionalStuff/Option/Option.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace FunctionalStuff.Option
 {
@@ -8,13 +9,22 @@ namespace FunctionalStuff.Option
         {
         }
 
-        public Option<TOut> Map<TOut>(Func<T, TOut> func)
+        public static Option<T> Some(T value)
         {
-            return this switch
+            return Some<T>.Create(value);
+        }
+
+        public static Option<T> None()
+        {
+            return None<T>.Create();
+        }
+        
+        public static Option<T> FromNullable(T input)
+        {
+            return input switch
                    {
-                       None<T> => None<TOut>.Create(),
-                       Some<T> s => Some<TOut>.Create(func(s.Value)),
-                       _ => throw new ArgumentOutOfRangeException()
+                       null => None(),
+                       { } some => Some(some)
                    };
         }
 
@@ -22,20 +32,20 @@ namespace FunctionalStuff.Option
         {
             return this switch
                    {
-                       None<T> => None<TOut>.Create(),
+                       None<T> => Option<TOut>.None(),
                        Some<T> s => func(s.Value),
                        _ => throw new ArgumentOutOfRangeException()
                    };
         }
 
-        public TOut Fold<TOut>(TOut state, Func<TOut, T, TOut> func)
+        public bool Contains(T value)
         {
-            return this switch
-                   {
-                       None<T> => state,
-                       Some<T> s => func(state, s.Value),
-                       _ => throw new ArgumentOutOfRangeException()
-                   };
+            return this is Some<T> s && s.Value.Equals(value);
+        }
+        
+        public int Count()
+        {
+            return this is Some<T> ? 1 : 0;
         }
 
         public T DefaultValue(T def)
@@ -44,22 +54,137 @@ namespace FunctionalStuff.Option
                    {
                        None<T> => def,
                        Some<T> s => s.Value,
+                       _ => throw new ArgumentException()
+                   };
+        }
+
+        public T DefaultWith(Func<T> defThunk)
+        {
+            return this switch
+                   {
+                       Some<T> s => s.Value,
+                       None<T> => defThunk(),
+                       _ => throw new ArgumentException(),
+                   };
+        }
+        
+        public bool Exists(Func<T, bool> predicate)
+        {
+            return this is Some<T> s && predicate(s.Value);
+        }
+
+        public Option<T> Filter(Func<T, bool> predicate)
+        {
+            return this switch
+                   {
+                       None<T> n => n,
+                       Some<T> s => predicate(s.Value) ? s : None(),
+                       _ => throw new ArgumentException()
+                   };
+        }
+
+        public TOut Fold<TOut>(TOut state, Func<TOut, T, TOut> folder)
+        {
+            return this switch
+                   {
+                       None<T> => state,
+                       Some<T> s => folder(state, s.Value),
                        _ => throw new ArgumentOutOfRangeException()
                    };
         }
 
-        public static Option<T> FromNullable(T input)
+        public TOut FoldBack<TOut>(Func<T, TOut, TOut> folder, TOut state)
         {
-            return input switch
+            return this switch
                    {
-                       null => None<T>.Create(),
-                       { } some => Some<T>.Create(some)
+                       None<T> => state,
+                       Some<T> s => folder(s.Value, state),
+                       _ => throw new ArgumentException()
+                   };
+        }
+        
+        public bool IsNone()
+        {
+            return this is None<T>;
+        }
+
+        public bool IsSome()
+        {
+            return this is Some<T>;
+        }
+
+        public void Iter(Action<T> action)
+        {
+            if (this is Some<T> s)
+                action(s.Value);
+        }
+        
+        public Option<TOut> Map<TOut>(Func<T, TOut> func)
+        {
+            return this switch
+                   {
+                       None<T> => Option<TOut>.None(),
+                       Some<T> s => Option<TOut>.Some(func(s.Value)),
+                       _ => throw new ArgumentOutOfRangeException()
                    };
         }
 
-        public static bool IsNone(Option<T> option)
+        public Option<TOut> Map2<T2, TOut>(Func<T, T2, TOut> mapping, Option<T2> other)
         {
-            return option is None<T>;
+            return (this, other) switch
+                   {
+                       (Some<T> s1, Some<T2> s2) => Option<TOut>.Some(mapping(s1.Value, s2.Value)),
+                       _ => Option<TOut>.None(),
+                   };
+        }
+
+        public Option<TOut> Map3<T2, T3, TOut>(Func<T, T2, T3, TOut> mapping, Option<T2> other1, Option<T3> other2)
+        {
+            return (this, other1, other2) switch
+                   {
+                       (Some<T> s1, Some<T2> s2, Some<T3> s3) =>
+                           Option<TOut>.Some(mapping(s1.Value, s2.Value, s3.Value)),
+                       _ => Option<TOut>.None(),
+                   };
+        }
+
+        public Option<T> OrElse(Option<T> ifNone)
+        {
+            return IsNone() ? ifNone : this;
+        }
+
+        public Option<T> OrElseWith(Func<Option<T>> ifNoneThunk)
+        {
+            return IsNone() ? ifNoneThunk() : this;
+        }
+
+        public T[] ToArray()
+        {
+            if (this is Some<T> s)
+            {
+                return new[] {s.Value};
+            }
+
+            return Array.Empty<T>();
+        }
+
+        public List<T> ToList()
+        {
+            if (this is Some<T> s)
+            {
+                return new List<T> {s.Value};
+            }
+
+            return new List<T>();
+        }
+        
+        public T UnwrapOr(Exception exception)
+        {
+            return this switch
+                   {
+                       Some<T> s => s.Value,
+                       _ => throw exception,
+                   };
         }
     }
 }
